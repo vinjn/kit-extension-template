3d1a509a24548cd97258858aadd64dd1c0fac3d7
vincentz <vincentz@nvidia.com>
2022-11-28 11:28:02 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,9 @@
-app\kit\omni.app.dev.bat --ext-folder exts --enable omni.hello.world
+%~dp0\_build\kit\omni.app.dev.bat ^
+    --ext-folder %~dp0\exts ^
+    --ext-folder %~dp0\app\exts ^
+    --ext-folder %~dp0\app\extscache ^
+    --enable omni.kit.profiler.window ^
+    --enable omni.kit.debug.vscode ^
+    --enable omni.kit.browser.sample ^    
+    --enable omni.hello.world ^
+    %*
